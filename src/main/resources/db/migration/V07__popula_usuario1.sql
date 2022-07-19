0f8f77d697f028b4bbf78182302177abdb20283a
TadeuViegas <tadeucosta@acad.ifma.edu.br>
2022-07-18 22:30:31 -0300

    update: add usuarios

---
+++ src/main/resources/db/migration/V07__popula_usuario1.sql
@@ -0,0 +1,4 @@
+INSERT INTO `usuario` (`email`,`papel`,`senha`) VALUES ('tadeu@email.com','ADMIN','tadeu123');
+INSERT INTO `usuario` (`email`,`papel`,`senha`) VALUES ('joao@email.com','ADMIN','joao2022');
+INSERT INTO `usuario` (`email`,`papel`,`senha`) VALUES ('tiago@email.com','ADMIN','123456');
+INSERT INTO `usuario` (`email`,`papel`,`senha`) VALUES ('diego@email.com','ADMIN','di1992');
